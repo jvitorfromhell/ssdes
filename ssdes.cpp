035db701a15b73a2d0ab972599e88076cb0e74e5
jvitorfromhell <jvitor.schneid@gmail.com>
2016-10-14 14:29:48 -0300

    Função E implementada

--- ssdes.cpp
+++ ssdes.cpp
@@ -22,9 +22,20 @@ SBox::SBox(int lines, int columns, string filename) {
 }
 
 SBox::~SBox() {
-
+	for (int i = 0; i < lines; i++) {
+		delete(matrix[i]);
+	}
+	delete(matrix);
 }
 
 int SBox::get(int position) {
 	return matrix[position & 8 >> 3][position & 7];
+}
+
+int E(int Di_minus_one) {
+	int first_two_bits = (Di_minus_one & 48) >> 4; 
+	int third_bit = (Di_minus_one & 8) >> 3;
+	int fourth_bit = (Di_minus_one & 4) >> 2;
+	int last_two_bits = Di_minus_one & 3;
+	return (first_two_bits << 6) | (fourth_bit << 5) | (third_bit << 4) | (fourth_bit << 3) | (third_bit << 2) | (last_two_bits);
 }
--- ssdes.h
+++ ssdes.h
@@ -12,3 +12,5 @@ public:
 
 	int get(int key);
 };
+
+int E(int Di_minus_one);
